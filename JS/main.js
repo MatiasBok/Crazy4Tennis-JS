24fc53bf49c5a388c92bc1a40f63c8e41afb0754
MatiasBok <matbuken@gmail.com>
2022-01-04 22:09:59 -0300

    Quinto Commit: Funcionando Formulario y creación de Card en RESERVAS; funciona formulario de INGRESO, en INGRESO

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,13 @@
 /**************CARDS de NUESTROS PROFES**************/
 
-let divProfes = document.getElementById("divProfes")
-
-fetch ('../nuestrosProfes.json')
-    .then(promesa => promesa.json())
-    .then(datosProfes => {
-        datosProfes.forEach(nuestrosProfes =>
-            divProfes.innerHTML +=`
+let divProfes = document.getElementById("divProfes");
+if (divProfes) {
+  fetch("../nuestrosProfes.json")
+    .then((promesa) => promesa.json())
+    .then((datosProfes) => {
+      datosProfes.forEach(
+        (nuestrosProfes) =>
+          (divProfes.innerHTML += `
             <div class="col-lg-4 col-md-3 col-sm-12">
               <div class="card h-30" id="nuestrosProfes${nuestrosProfes.id}">
                   <img src="../assets/cv/${nuestrosProfes.img}" class="card-img-top">
@@ -21,12 +22,13 @@ fetch ('../nuestrosProfes.json')
                   </div>
               </div>
             </div> 
-            `
-            );
+            `)
+      );
     })
-    .catch(error => console.error(error))
+    .catch((error) => console.error(error));
+}
 
-  /***************RESERVAS***************/
+/***************RESERVAS***************/
 
 class Jugador {
   /*por convención las clases van siempre en mayúscula*/
@@ -42,8 +44,8 @@ class Jugador {
     ciudad,
     provincia,
     codigoPostal,
-    password,
-    repeatPassword
+    contraseña,
+    repeatContraseña
   ) {
     this.nombre = nombre;
     this.apellido = apellido;
@@ -56,20 +58,21 @@ class Jugador {
     this.ciudad = ciudad;
     this.provincia = provincia;
     this.codigoPostal = codigoPostal;
-    this.password = password;
-    this.repeatPassword = repeatPassword;
+    this.contraseña = contraseña;
+    this.repeatContraseña = repeatContraseña;
   }
 
   devolverDatos() {
-    return `${this.nombre} - ${this.apellido} - ${this.DNI} - ${this.email} - ${this.nombreDeUsuario} - ${this.sexo} - ${this.fechaDeNacimiento} - ${this.mobil} - ${this.ciudad} - ${this.provincia} - ${this.codigoPostal} - ${this.password} - ${this.repeatPassword}`;
+    return `${this.nombre} - ${this.apellido} - ${this.DNI} - ${this.email} - ${this.nombreDeUsuario} - ${this.sexo} - ${this.fechaDeNacimiento} - ${this.mobil} - ${this.ciudad} - ${this.provincia} - ${this.codigoPostal} - ${this.contraseña} - ${this.repeatContraseña}`;
   }
 }
 
 let jugadores = [];
 
 let BtnForm = document.getElementById("ButtonFormSubmit");
-if(BtnForm){
-  const validacion = (e) => {
+if (BtnForm) {
+ 
+  function validacion(e) {
     e.preventDefault();
     const nombre = document.getElementById("nombre");
     const apellido = document.getElementById("apellido");
@@ -86,257 +89,273 @@ if(BtnForm){
     const repeatContraseña = document.getElementById("repeatContraseña");
     const invalidCheck = document.getElementById("invalidCheck");
 
-    if(nombre.value === ""){
-      $('#faltaNombre').show();
+    if (nombre.value === "") {
+      $("#faltaNombre").show();
       nombre.focus();
       return false;
-    }else{
-      $('#faltaNombre').hide();
+    } else {
+      $("#faltaNombre").hide();
       nombre.focus();
     }
 
-    if(apellido.value === ""){
-      $('#faltaApellido').show();
+    if (apellido.value === "") {
+      $("#faltaApellido").show();
       apellido.focus();
       return false;
-    }else{
-      $('#faltaApellido').hide();
+    } else {
+      $("#faltaApellido").hide();
       apellido.focus();
     }
 
-    if(numeroDeDocumento.value === ""){
-      $('#faltaDocumento').show();
+    if (numeroDeDocumento.value === "") {
+      $("#faltaDocumento").show();
       numeroDeDocumento.focus();
       return false;
-    }else{
-      $('#faltaDocumento').hide();
+    } else {
+      $("#faltaDocumento").hide();
       numeroDeDocumento.focus();
     }
 
-    if(email.value === ""){
-      $('#faltaEmail').show();
+    if (email.value === "") {
+      $("#faltaEmail").show();
       email.focus();
       return false;
-    }else{
-      $('#faltaEmail').hide();
+    } else {
+      $("#faltaEmail").hide();
       email.focus();
     }
     if (!emailValido(email.value)) {
-      $('#errorEmail').show();
+      $("#errorEmail").show();
       email.focus();
-      return false; 
-    }else{
-      $('#errorEmail').hide();
+      return false;
+    } else {
+      $("#errorEmail").hide();
       email.focus();
     }
 
-    if(nombreDeUsuario.value === ""){
-      $('#faltaNombreDeUsuario').show();
+    if (nombreDeUsuario.value === "") {
+      $("#faltaNombreDeUsuario").show();
       nombreDeUsuario.focus();
       return false;
-    }else{
-      $('#faltaNombreDeUsuario').hide();
-      nombreDeUsuario.focus();
-    }
-    if (!usuarioValido(nombreDeUsuario.value)){
-      $("#errorNombreDeUsuario").show();
-      nombreDeUsuario.focus();  
-      return false;
-    }else{
-      $('#errorNombreDeUsuario').hide();
+    } else {
+      $("#faltaNombreDeUsuario").hide();
       nombreDeUsuario.focus();
     }
 
-    if(sexo.value === ""){
-      $('#faltaSexo').show();
+    if (sexo.value === "") {
+      $("#faltaSexo").show();
       sexo.focus();
       return false;
-    }else{
-      $('#faltaSexo').hide();
+    } else {
+      $("#faltaSexo").hide();
       sexo.focus();
     }
 
-    if(fechaDeNacimiento.value === ""){
-      $('#faltaFechaDeNacimiento').show();
+    if (fechaDeNacimiento.value === "") {
+      $("#faltaFechaDeNacimiento").show();
       fechaDeNacimiento.focus();
       return false;
-    }else{
-      $('#faltaFechaDeNacimiento').hide();
+    } else {
+      $("#faltaFechaDeNacimiento").hide();
       fechaDeNacimiento.focus();
     }
 
-    if(mobil.value === ""){
-      $('#faltaMobil').show();
+    if (mobil.value === "") {
+      $("#faltaMobil").show();
       mobil.focus();
       return false;
-    }else{
-      $('#faltaMobil').hide();
+    } else {
+      $("#faltaMobil").hide();
       mobil.focus();
     }
 
-    if (!mobilValido(mobil.value)){
+    if (!mobilValido(mobil.value)) {
       $("#errorMobil").show();
-      mobil.focus();  
-    }else{
-      $('#errorMobil').hide();
+      mobil.focus();
+    } else {
+      $("#errorMobil").hide();
       mobil.focus();
     }
 
-    if(ciudad.value === ""){
-      $('#faltaCiudad').show();
+    if (ciudad.value === "") {
+      $("#faltaCiudad").show();
       ciudad.focus();
       return false;
-    }else{
-      $('#faltaCiudad').hide();
+    } else {
+      $("#faltaCiudad").hide();
       ciudad.focus();
     }
 
-    if(provincia.value === ""){
-      $('#faltaProvincia').show();
+    if (provincia.value === "") {
+      $("#faltaProvincia").show();
       provincia.focus();
       return false;
-    }else{
-      $('#faltaProvincia').hide();
+    } else {
+      $("#faltaProvincia").hide();
       provincia.focus();
     }
 
-    if(codigoPostal.value === ""){
-      $('#faltaCodigoPostal').show();
+    if (codigoPostal.value === "") {
+      $("#faltaCodigoPostal").show();
       codigoPostal.focus();
       return false;
-    }else{
-      $('#faltaCodigoPostal').hide();
+    } else {
+      $("#faltaCodigoPostal").hide();
       codigoPostal.focus();
     }
 
-    if(contraseña.value === ""){
-      $('#faltaContraseña').show();
+    if (contraseña.value === "") {
+      $("#faltaContraseña").show();
       contraseña.focus();
       return false;
-    }else{
-      $('#faltaContraseña').hide();
+    } else {
+      $("#faltaContraseña").hide();
       contraseña.focus();
-    }
-    if (!contraseñaValido(contraseña.value)){
-      $("#errorContraseña").show();
-      contraseña.focus();  
-    }else{
-      $('#errorContraseña').hide();
-      contraseña.focus();
+    }  
+
+    if (repeatContraseña.value === "") {
+      $("#faltaRepeatContraseña").show();
+      repeatContraseña.focus();
+      return false;
+    } else {
+      $("#faltaRepeatContraseña").hide();
+      repeatContraseña.focus();
     }
 
-    if(repeatContraseña.value === ""){
-      $('#faltaRepeatContraseña').show();
-      repeatContraseña.focus();
-      return false;
-    }else{
-      $('#faltaRepeatContraseña').hide();
-      repeatContraseña.focus();
-    }
-    if (!repeatContraseñaValido(repeatContraseña.value)){
-      $("#errorRepeatContraseña").show();
-      repeatContraseña.focus();  
-    }else{
-      $('#errorRepeatContraseña').hide();
-      repeatContraseña.focus();
-    }
-    
-    if(invalidCheck.value === ""){
-      $('#faltaAceptar').show();
+    if (invalidCheck.checked === false) {
+      $("#faltaAceptar").show();
       invalidCheck.focus();
       return false;
-    }else{
-      $('#faltaAceptar').hide();
+    } else {
+      $("#faltaAceptar").hide();
       invalidCheck.focus();
     }
-    return true;
-  }
+    return true && verificarContraseñas();;
 
-  const emailValido = email => {
-    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-    }
-  const usuarioValido = nombreDeUsuario => {
-    return /^[a-z0-9_-]{5,10}$/.test(nombreDeUsuario);
-    }
-  const mobilValido = mobil => {
-    return /^[0-9]{10}$/.test(mobil);
-    }
-  const contraseñaValido = contraseña => {
-    return /^.*(?=.{8,}) (?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%&]).*$/.test(contraseña);
-    }
-  const repeatContraseñaValido = repeatContraseña => {
-    return /^.*(?=.{8,}) (?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%&]).*$/.test(repeatContraseña);
-    }
+  };
+
+  BtnForm.addEventListener("click", (e) => {
+    e.preventDefault();
+    const FormDatos = document.getElementById("crearUsuario");
+    const ContenedorUsers = document.getElementById("nuevoUsuario");
+    if(validacion(e)){
+      ContenedorUsers.innerHTML = "";
+      ContenedorUsers.innerHTML = `
+
+      <div class="container">
+      <div class="row">
+        <div class="col-lg-7 cardFormulario">
+          <div class="card cardFormRes">
+            <div class="card-header">
+              <h4 class= "cardtit">Datos del usuario</h4>
+            </div>
+            <div class="card-body">
+              <div class="row">
+                <div class="col-lg-7">
+                  <div class="form-group">
+                    <label class="titSecc" for="nombre">Nombre</label>
+                    <input type="text" class="form-control" id="nombre" value="${nombre.value}" disabled>
+                  </div>
+                  <div class="form-group">
+                    <label class="titSecc" for="apellido">Apellido</label>
+                    <input type="text" class="form-control" id="apellido" value="${apellido.value}" disabled>
+                  </div>
+                  <div class="form-group">
+                    <label class="titSecc" for="numeroDeDocumento">Número de documento</label>
+                    <input type="text" class="form-control" id="numeroDeDocumento" value="${numeroDeDocumento.value}" disabled>
+                  </div>
+                  <div class="form-group">
+                    <label class="titSecc" for="email">Email</label>
+                    <input type="text" class="form-control" id="email" value="${email.value}" disabled>
+                  </div>
+                  <div class="form-group">
+                    <label class="titSecc" for="nombreDeUsuario">Nombre de usuario</label>
+                    <input type="text" class="form-control" id="nombreDeUsuario" value="${nombreDeUsuario.value}" disabled>
+                  </div>
+                  <div class="form-group">
+                    <label class="titSecc" for="sexo">Sexo</label>
+                    <input type="text" class="form-control" id="sexo" value="${sexo.value}" disabled>
+                  </div>
+                  <div class="form-group">
+                    <label class="titSecc" for="fechaDeNacimiento">Fecha de Nacimiento</label>
+                    <input type="text" class="form-control" id="fechaDeNacimiento" value="${fechaDeNacimiento.value}" disabled>
+                  </div>
+                  <div class="form-group">
+                    <label class="titSecc" for="mobil">Móbil</label>
+                    <input type="text" class="form-control" id="mobil" value="${mobil.value}" disabled>
+                  </div>
+                  <div class="form-group">
+                    <label class="titSecc" for="ciudad">Ciudad</label>
+                    <input type="text" class="form-control" id="ciudad" value="${ciudad.value}" disabled>
+                  </div>
+                  <div class="form-group">
+                    <label class="titSecc" for="provincia">Provincia</label>
+                    <input type="text" class="form-control" id="provincia" value="${provincia.value}" disabled>
+                  </div>
+                  <div class="form-group">
+                    <label class="titSecc" for="codigoPostal">Código Postal</label>
+                    <input type="text" class="form-control" id="codigoPostal" value="${codigoPostal.value}" disabled>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>        
+      </div>
+    </div>
+
+      `
   
-  BtnForm.addEventListener("click", validacion);
+      FormDatos.reset();
+    }else{
+      e.preventDefault();
+      
+    }
+  });
+
 }
 
-document.getElementById ("ButtonFormSubmit").addEventListener('click',() => {
-  fetch ("")
-  then (response => response.json())
-  then(data=> {
-    nuevoUsuario.innerHTML = Object.entries(data).filter(usuario => usuario[0] !="password","repeatPassword").map
-    (usuario => usuario= `
-      <div class="card text-white bg-primary mb-3" style="max-width: 20rem;">
-        <div class="card-header">${usuario.nombre}.toUpperCase() ${usuario.apellido}.toUpperCase()</div>
-        <div class="card-body">
-          <p class="card-text">Número de documento: ${usuario.documento}</p>
-          <p class="card-text">Email: ${usuario.email}</p>
-          <p class="card-text">Nombre de usuario: ${usuario.nombreDeUsuario}</p>
-          <p class="card-text">Sexo: ${usuario.sexo}</p>
-          <p class="card-text">Fecha de nacimiento: ${usuario.fechaDeNacimiento}</p>
-          <p class="card-text">Móbil: ${usuario.mobil}</p>
-          <p class="card-text">Ciudad:${usuario.ciudad}</p>
-          <p class="card-text">Provincia: ${usuario.provincia}</p>
-          <p class="card-text">Código Postal: ${usuario.codigoPostal}</p>
-        </div>
-      </div>
-    `
-    ).join(" ")
-  })
-})
+const emailValido = (email) => {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+};
+const mobilValido = (mobil) => {
+  return /^[0-9]{10}$/.test(mobil);
+};
+const contraseñaValido = (contraseña) => {
+  return /^.*(?=.{8,}) (?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%&]).*$/.test(contraseña);
+};
+const repeatContraseñaValido = (repeatContraseña) => {
+  return /^.*(?=.{8,}) (?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%&]).*$/.test(repeatContraseña);
+};
+
 
 function verificarContraseñas() {
- 
-  contraseña = document.getElementById('contraseña');
-  repeatContraseña = document.getElementById('repeatContraseña');
+  contraseña = document.getElementById("contraseña");
+  repeatContraseña = document.getElementById("repeatContraseña");
 
   // Verificamos si las constraseñas no coinciden
   if (contraseña.value != repeatContraseña.value) {
- 
-  // Si las constraseñas no coinciden mostramos un mensaje
-  document.getElementById("error").classList.add("mostrar");
+    // Si las contraseñas no coinciden mostramos un mensaje
+    document.getElementById("error").style.display="block";
+    return false;
 
-  return false;
+  } else {
+    // Si las contraseñas coinciden ocultamos el mensaje de error
+    document.getElementById("error").style.display="none";
 
-} 
+    // Mostramos un mensaje mencionando que las Contraseñas coinciden
+    document.getElementById("ok").style.display="block";
 
-else {
- 
-  // Si las contraseñas coinciden ocultamos el mensaje de error
-  document.getElementById("error").classList.add("ocultar");
-
-  // Mostramos un mensaje mencionando que las Contraseñas coinciden 
-  document.getElementById("ok").classList.add("mostrar");
-
-  // Desabilitamos el botón de submit
-  document.getElementById("ButtonFormSubmit").disabled = true;
-
-  // Refrescamos la página (Simulación de envío del formulario) 
-  setTimeout(function() {
-      location.reload();
-  }, 3000);
-
-  return true;
+    return true;
+  }
 }
-}  
-
 
 /**********VALIDACIÓN DE FORMULARIO DE INGRESO**********/
 
 var attempt = 3; // Variable to count number of attempts.
 
 function validate() {
+
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
 
@@ -347,7 +366,7 @@ function validate() {
     return false;
   } else {
     attempt--; // Decrementing by one.
-    alert("A usted le quedan " + attempt + " intentos;");
+    alert("Contraseña incorrecta. A usted le quedan " + attempt + " intentos;");
     // Disabling fields after 3 attempts.
     if (attempt == 0) {
       document.getElementById("username").disabled = true;
@@ -357,4 +376,3 @@ function validate() {
     }
   }
 }
-      
